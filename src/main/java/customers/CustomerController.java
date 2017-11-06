739823532d589214e34e8aa70fbb222baaea8086
GeneCPChung <genecpc@gmail.com>
2017-11-06 15:30:06 -0500

    create files

---
+++ src/main/java/customers/CustomerController.java
@@ -0,0 +1,21 @@
+package customers;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomerController {
+
+	@Resource
+	CustomerRepository customerRepo;
+
+	@RequestMapping("/customers")
+	public String getAllReviews(Model model) {
+		model.addAttribute("customers", customerRepo.findAll());
+		return "customers";
+	}
+
+}
